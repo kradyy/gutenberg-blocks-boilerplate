cba398c3e54d292d07cd2dbf9f1fa37fdb0a0086
Chris Johansson <chris.johansson@mildmedia.se>
2022-11-06 22:02:46 +0100

    Truncate boilerplate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
 import { registerBlockType } from '@wordpress/blocks';
 import './style.scss';
-import Edit from './edit';
-import save from './save';
+import './editor.scss';
 import metadata from './block.json';
 
-registerBlockType( metadata.name, {
-	edit: Edit,
-	save,
-} );
+registerBlockType(metadata.name, {
+	edit: () => {
+		const blockProps = useBlockProps();
+
+		return (
+			<p {...blockProps}>
+				{__('Boilerplate - hello from the editor!', 'boilerplate')}
+			</p>
+		);
+	},
+	save: () => {
+		const blockProps = useBlockProps.save();
+
+		return (
+			<p {...blockProps}>
+				{__('Boilerplate - hello from the editor!', 'boilerplate')}
+			</p>
+		);
+	},
+});
